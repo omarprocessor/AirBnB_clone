ef3d4cccc5514607ec007f2019c9a33f4ee770a2
omarprocessor <wadaniinarais1@gmail.com>
2024-11-17 00:38:30 -0800

    task done

---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""
+Moduli ya: __init__.py
+"""
--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,59 @@
 #!/usr/bin/python3
+"""
+Moduli: base_model.py
+"""
+import models
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """Husimamia sifa na mbinu za kawaida kwa madarasa mengine."""
+    """
+    Darasa la msingi ambalo linafafanua sifa/methodi
+    za kawaida kwa madarasa mengine.
+    """
 
-    def __init__(self):
-        """Hutengeneza mfano mpya wa BaseModel."""
+    def __init__(self, *args, **kwargs):
+        """
+        Huanzisha kitu kipya pamoja na sifa zake.
+        """
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+            return
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+        models.storage.new(self)
+
     def __str__(self):
-        """Hurejesha maandishi ya uwakilishi wa mfano."""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """
+        Hurejesha uwakilishi wa maandishi wa mfano.
+        """
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """Husasisha sifa ya updated_at na tarehe ya sasa."""
+        """
+        Husasisha sifa ya umma `updated_at` na tarehe ya sasa.
+        """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        """Hurejesha kamusi ya uwakilishi wa mfano."""
-        obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = self.__class__.__name__
-        obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
-        return obj_dict
+        """
+        Hurejesha kamusi yenye funguo/maadili yote
+        ya __dict__ ya mfano.
+        """
+        dict = {**self.__dict__}
+        dict['__class__'] = type(self).__name__
+        dict['created_at'] = dict['created_at'].isoformat()
+        dict['updated_at'] = dict['updated_at'].isoformat()
+
+        return dict
