0eb71d97d1183e3edbded7de5185e07980fe575b
omarprocessor <wadaniinarais1@gmail.com>
2024-11-17 11:52:24 -0800

    task done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,73 +1,93 @@
 #!/usr/bin/python3
-"""
-Moduli: file_storage.py
-
-Inafafanua darasa la `FileStorage`.
-"""
-import os
+"""Moduli ya darasa FileStorage."""
+import datetime
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.review import Review
-from models.amenity import Amenity
-from models.place import Place
+import os
 
 
-class FileStorage():
-    """
-    inafanya serializes ya instances katika faili la JSON na
-    deserializes faili la JSON kuwa instances
-    """
-
+class FileStorage:
+    """Darasa la kuhifadhi na kurejesha data."""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-        inarudisha dictionary __objects
-        """
+        """Inarudisha kamusi ya __objects."""
         return FileStorage.__objects
 
     def new(self, obj):
-        """
-        inafuta katika __objects obj na ufunguo <jina la darasa la obj>.id
-        """
+        """Inaweka kwenye __objects kitu chenye ufunguo <jina la darasa>.id."""
         key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """
-        inafanya serializes __objects katika faili la JSON (njia: __file_path)
-        """
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(
-                {key: value.to_dict() for key, value in FileStorage.__objects.items()}, f)
+        """Inabadilisha __objects kuwa faili la JSON (njia: __file_path)."""
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(d, f)
+
+    def classes(self):
+        """Inarudisha kamusi ya madarasa halali na marejeleo yao."""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
 
     def reload(self):
-        """
-        inafanya deserializes faili la JSON kuwa __objects tu ikiwa faili la JSON
-        linapatikana; vinginevyo, halifanyi chochote
-        """
-        current_classes = {'BaseModel': BaseModel, 'User': User,
-                           'Amenity': Amenity, 'City': City, 'State': State,
-                           'Place': Place, 'Review': Review}
-
-        if not os.path.exists(FileStorage.__file_path):
+        """Inapakia tena vitu vilivyohifadhiwa."""
+        if not os.path.isfile(FileStorage.__file_path):
             return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {k: self.classes()[v["__class__"]](**v)
+                        for k, v in obj_dict.items()}
+            FileStorage.__objects = obj_dict
 
-        with open(FileStorage.__file_path, 'r') as f:
-            deserialized = None
-
-            try:
-                deserialized = json.load(f)
-            except json.JSONDecodeError:
-                pass
-
-            if deserialized is None:
-                return
-
-            FileStorage.__objects = {
-                key: current_classes[key.split('.')[0]](**value)
-                for key, value in deserialized.items()}
+    def attributes(self):
+        """Inarudisha sifa halali na aina zao kwa jina la darasa."""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
